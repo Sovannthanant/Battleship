eb7b65eee5d521a05487fe2e3cd81840565d6e9e
2472525 <2472525@cegepoutaouais.qc.ca>
2025-10-30 13:58:13 -0400

    J'ai créer la liste des différents bateaux.

--- Battleship.py
+++ Battleship.py
@@ -2,8 +2,8 @@
 #----------------------------------------BATTLESHIP----------------------------------------🚢💥
 #Mon Pseudo-Code pour un jeu de Battleship fonctionnel.
 #1. Créer le tableau du premier joueur et deuxième joueur.
-#   -Grille des bateau du premier joueur et grille de tir.
-#   -Grille des bateau du deuxieme joueur et grille de tir.
+#   -Grille des bateau du premier joueur et son grille de tir.
+#   -Grille des bateau du deuxieme joueur et son grille de tir.
 #2. Créer les dimensions des cinq différents navires:
 #   -Un Tourpilleur, dimension de 1x2 espaces.
 #   -Deux Croiseur, dimensions de 1x3 espaces.
@@ -21,7 +21,7 @@
 #1. Créer le tableau du premier joueur et deuxième joueur.
 #   -Grille des bateau du premier joueur et grille de tir.
 grille_bateau_j1 = [
-    ["I=====I GRILLE DU JOUEUR 1 I=====I"],
+    ["I=====I BATEAUX DU JOUEUR 1 I=====I"],
     ["  "," A"," B"," C"," D"," E"," F"," G"," H"," I"," J"],
     ["01","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
     ["02","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
@@ -35,7 +35,7 @@ grille_bateau_j1 = [
     ["10","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
 ]
 grille_tir_j1 = [
-    ["I=====I GRILLE DU JOUEUR 1 I=====I"],
+    ["I=======I TIR DU JOUEUR 1 I=======I"],
     ["  "," A"," B"," C"," D"," E"," F"," G"," H"," I"," J"],
     ["01","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
     ["02","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
@@ -50,7 +50,7 @@ grille_tir_j1 = [
 ]
 #   -Grille des bateau du deuxìème joueur et grille de tir.
 grille_bateau_j2 = [
-    ["I=====I GRILLE DU JOUEUR 2 I=====I"],
+    ["I=====I BATEAUX DU JOUEUR 2 I=====I"],
     ["  "," A"," B"," C"," D"," E"," F"," G"," H"," I"," J"],
     ["01","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
     ["02","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
@@ -64,7 +64,7 @@ grille_bateau_j2 = [
     ["10","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
 ]
 grille_tir_j2 = [
-    ["I=====I GRILLE DU JOUEUR 2 I=====I"],
+    ["I=======I TIR DU JOUEUR 2 I=======I"],
     ["  "," A"," B"," C"," D"," E"," F"," G"," H"," I"," J"],
     ["01","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
     ["02","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦",],
@@ -81,24 +81,28 @@ grille_tir_j2 = [
 #2. Créer les dimensions des cinq différents navires:
 #   -Un Tourpilleur, dimension de 1x2 espaces.
 tourpilleur_axe_x = ["1","1"]
+tourpilleur_placer = False
 #   -Deux Croiseur, dimensions de 1x3 espaces.
 croiseur1_axe_x = ["1","1","1"]
 croiseur2_axe_x = ["1","1","1"]
+croiseur1_placer = False
+croiseur2_placer = False
 #   -Un Cuirassé, dimension de 1x4 espaces.
 cuirasse_axe_x = ["1","1","1","1"]
+cuirasse_placer = False
 #   -Un Porte-Avion, dimension de 1x5 cases.
 porte_avion_axe_x = ["1","1","1","1","1"]
+porte_avion_placer = False
+bateaux_placer = tourpilleur_placer and croiseur1_placer and croiseur2_placer and cuirasse_axe_x and porte_avion_axe_x
 
 #3. Faire Demander aux deux joueurs de placer les bateau dans la grille:
 #   -Offrir le choix au joueur s'il veut tourner la pièce dans l'angle des x ou y.
-for ligne in grille_bateau_j1:
-    print(*ligne)
+
 print("Veuillez placer le tourpilleur dans votre grille (pièce de deux cases).")
 
+print("")
 for ligne in grille_bateau_j1:
-    ligne.append(tourpilleur_axe_x)
     print(*ligne)
 
-print("")
 for ligne in grille_tir_j1:
     print(*ligne)
